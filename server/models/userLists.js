6ec6f78242cc8188c3f219b101ba0a086211ac53
Legallait <112770080+Legallait@users.noreply.github.com>
2024-12-01 16:45:11 +0100

    ajout de UserList

--- server/models/userLists.js
+++ server/models/userLists.js
@@ -1,25 +1,27 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('user_lists', {
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true, // Clé primaire composite avec game_list_id
-            references: {
-                model: 'users', // Table `users`
-                key: 'user_id'
-            }
-        },
-        game_list_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true, // Clé primaire composite avec user_id
-            references: {
-                model: 'game_lists', // Table `game_lists`
-                key: 'game_list_id'
-            }
-        }
-    }, {
-        freezeTableName: true, // Utilise exactement le nom de la table `user_lists`
-        timestamps: false // Pas de colonnes createdAt/updatedAt
-    });
+  const userLists = sequelize.define('user_lists', {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, // Ajoutez ceci
+      references: {
+        model: 'users',
+        key: 'user_id',
+      },
+    },
+    game_list_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, // Et ceci
+      references: {
+        model: 'game_lists',
+        key: 'game_list_id',
+      },
+    },
+  }, {
+    freezeTableName: true,
+    timestamps: false,
+  });
+
+  return userLists;
 };
