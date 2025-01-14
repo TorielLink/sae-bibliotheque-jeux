41a6e99eb56eeb9445ed1ffd0e357fbfa583745b
BaanuSha <siva.baanusha@gmail.com>
2025-01-14 23:49:09 +0100

    Catalogue de jeux
     - composant GameCardCategory :
        propre à Catalogue
     - composant PodiumCategory :
        affichage des jeux avec classement
        podium
     - composant GameCategory :
       affichage des jeux en liste ou avec
       podium (tous sans podium)
     - jeu triés par genre (affichage 5 mais
       sinon 8)

--- client/src/components/GameCardCategory.jsx
+++ client/src/components/GameCardCategory.jsx
@@ -1,64 +1,126 @@
-import React from 'react';
+import React from "react";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
 import StarIcon from "@mui/icons-material/Star";
+import Button from "@mui/material/Button";
+import { alpha } from "@mui/material/styles";
 
-import { useTheme } from '@mui/material/styles';
+import { useTheme } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
 
+const GameCardCategory = ({ id, image, title, note, tags }) => {
+  const theme = useTheme();
+  const styles = getStyles(theme);
+  const navigate = useNavigate();
 
-const JeuPopulaire = ({ image, title, note, tags }) => {
-    const theme = useTheme();
-    const styles = getStyles(theme); // import style
+  // Débogage : vérifier les props
+  //console.log({ id, image, title, note, tags });
+
+  // permet d'accéder directement à la page détails du jeu
+  const gamesDetailsClick = () => {
+    console.log("Navigating to details with ID:", id);
+    navigate(`/details/${id}`);
+  };
 
   return (
-    <section className="gameCard">
-      <img src={image} alt={title}  style={styles.poster} />
-      <h3 style={styles.title} >{title}</h3>
-      <div className="notes">
-        <p>Note: {note} </p> 
-        <StarIcon style={styles.star}/>
+    <section style={styles.gameCard}>
+      <img src={image} alt={title} style={styles.poster} />
+
+      <div style={styles.infos}>
+        <h3 style={styles.title}>{title || "Titre non disponible"}</h3>
+        <div style={styles.tags}>
+          {tags.length > 0 ? (
+            tags.map((tag, index) => (
+              <Stack key={index} direction="row" spacing={1}>
+                <Chip label={tag} size="small" style={styles.tag} />
+              </Stack>
+            ))
+          ) : (
+            <p>Aucun tag disponible</p>
+          )}
+        </div>
+        <div style={styles.notes}>
+          <p style={styles.note}>Note : {note || ""} </p>
+          <StarIcon style={styles.star} />
+        </div>
+
+        <Button style={styles.button} onClick={gamesDetailsClick}>
+          Découvrir
+        </Button>
       </div>
-      <div className="tags">
-        {tags.map((tag, index) => (
-          <span key={index} className="tag" style ={styles.tags}>{tag} </span>
-        ))}
-      </div>
-      <button>Découvrir</button>
     </section>
   );
 };
 
 const getStyles = (theme) => ({
-    poster: {
-        width: "9.375em", //13em
-        height: "14.063em", //20em
-        borderRadius: "0.3125em",
-        boxShadow: "0 0 0.5em #000000",
-        overflow: "hidden",
-        cursor: "pointer",
-    },
-    title :{
-        fontSize: "20px",
-        fontWeight:"500",
-        fontFamily : theme.typography.fontFamily,
-        color : theme.palette.text.primary,
-    },
-    star : {
-        height : "1em",
-        weight : "1em",
-        color : theme.palette.colors.yellow,
-    },
-    note : {
-        fontSize: "15px",
-        fontFamily : theme.typography.fontFamily,
-        color : theme.palette.text.primary,
-    },
-    tags : {
-        fontSize: "15px",
-        fontFamily : theme.typography.fontFamily,
-        color : theme.palette.text.contrast,
-        backgroundColor: theme.palette.colors.red,
-        padding : "0.438em",
-        borderRadius: "0.3125em",
-    }          
-})
+  gameCard: {
+    display: "flex",
+    alignItems: "start",
+    height: "16em",
+    width: "26em",
+    position :"relative",
+  },
+  poster: {
+    width: "11em", //13em ou 9.375em
+    height: "inherit", //20em ou 14.063em
+    borderRadius: "2%",
+    boxShadow: "0 0 0.5em #000000",
+    overflow: "hidden",
+  },
+  infos: {
+    width: "15em",
+    height: "inherit",
+    marginLeft: "2%",
+  },
+  title: {
+    fontSize: "20px",
+    fontWeight: "700",
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.text.primary,
+    margin: "0 0 7% 0",
+  },
+  tags: {
+    display: "flex", // Utilisation de flexbox
+    alignItems: "center", // Alignement vertical centré
+    flexWrap: "wrap", // Autorise le passage à la ligne si nécessaire
+    gap: "0.25em",
+    margin: "0 0 2% 0",
+  },
+  tag: {
+    backgroundColor: theme.palette.colors.red,
+    color: theme.palette.text.contrast,
+    fontSize: "14px",
+    height: "1.5em",
+    borderRadius: "0.325em",
+    margin: "0 0.25em 0.25em 0",
+    overflow: "hidden",
+  },
+  notes: {
+    display: "flex",
+    margin: "0 0 10% 0",
+  },
+  note: {
+    fontSize: "15px",
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.text.primary,
+  },
+  star: {
+    color: theme.palette.colors.yellow,
+    margin: "5% 0 0 2%",
+  },
+  button: {
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.text.primary,
+    backgroundColor: alpha(theme.palette.colors.purple, 0.6), // 30% de transparence
+    borderColor: theme.palette.colors.purple,
+    padding: "0.5% 4%",
+    marginRight: "7%",
+    borderRadius: "15px",
+    fontSize: "13px",
+    position : "absolute",
+    bottom : "0",
+    right : "0",
+  },
+});
 
-export default JeuPopulaire;
+export default GameCardCategory;
--- client/src/pages/CataloguePage.jsx
+++ client/src/pages/CataloguePage.jsx
@@ -1,16 +1,245 @@
-import React from 'react';
-import GameCardCategory from '../components/GameCardCategory';
+import React, { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import GameCategory from "../components/GameCategory";
+import GameList from "../components/GameList.jsx";
 
-function CataloguePage() {
-  const genres = ["FPS","aventure"] // test
+import { useTheme } from "@mui/material/styles";
+
+const CataloguePage = () => {
+  const theme = useTheme();
+  const styles = getStyles(theme);
+
+  const [allGames, setAllGames] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  // Définir un état pour chaque catégorie
+    const [visibleCategory, setVisibleCategory] = useState({
+      fighting: false,
+      shooter: false,
+      platform: false,
+      rts: false,
+      rpg: false,
+      simulation: false,
+      strategy: false,
+      adventure: false,
+    });
+
+  // Data de chaque catégorie
+  const [fightingGames, setFightingGames] = useState([]);
+  const [shooterGames, setShooterGames] = useState([]);
+  const [platformGames, setPlatformGames] = useState([]);
+  const [rtsGames, setRtsGames] = useState([]);
+  const [rpgGames, setRpgGames] = useState([]);
+  const [simulationGames, setSimulationGames] = useState([]);
+  const [strategyGames, setStrategyGames] = useState([]);
+  const [adventureGames, setAdventureGames] = useState([]);
+
+  const catTitle = [
+    { id: 4, name: "Fighting" },
+    { id: 5, name: "Shooter" },
+    { id: 8, name: "Platform" },
+    { id: 11, name: "Real Time Strategy (RTS)" },
+    { id: 12, name: "Role-playing (RPG)" },
+    { id: 13, name: "Simulator" },
+    { id: 15, name: "Strategy" },
+    { id: 32, name: "Indie" },
+    { id: 31, name: "Adventure" },
+  ];
+
+  // ID des catégories
+  const categoryID = catTitle.map(function (category) {
+    return category.id;
+  });
+  console.log("ID des catégories :", categoryID);
+
+  // Nom des catégories
+  const categoryName = catTitle.map(function (category) {
+    return category.name;
+  });
+  console.log("Nom des catégories :", categoryName);
+
+  const fetchGameByGenres = async (body) => {
+    try {
+      const response = await fetch(`http://localhost:8080/games/by-genres`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      if (!response.ok) throw new Error(`Erreur API : ${response.statusText}`);
+      return await response.json();
+    } catch (error) {
+      console.error("Erreur lors de la récupération des jeux :", error);
+      throw error;
+    }
+  };
+
+  //mettre les jeux dans des variables
+  const filterGamesByGenre = (games) => {
+    console.log("Jeux avant filtrage :", games);
+    setFightingGames(
+      games.filter((game) => game.genres.includes(categoryName[0]))
+    );
+    setShooterGames(
+      games.filter((game) => game.genres.includes(categoryName[1]))
+    );
+    setPlatformGames(
+      games.filter((game) => game.genres.includes(categoryName[2]))
+    );
+    setRtsGames(games.filter((game) => game.genres.includes(categoryName[3])));
+    setRpgGames(games.filter((game) => game.genres.includes(categoryName[4])));
+    setSimulationGames(
+      games.filter((game) => game.genres.includes(categoryName[5]))
+    );
+    setStrategyGames(
+      games.filter((game) => game.genres.includes(categoryName[6]))
+    );
+    setAdventureGames(
+      games.filter((game) => game.genres.includes(categoryName[7]))
+    );
+  };
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      setLoading(true);
+      try {
+        const body = { limit: 100, offset: 0, genres: categoryID };
+        const gamesData = await fetchGameByGenres(body);
+        console.log("Données reçues API :", gamesData);
+  
+        // Une fois les jeux récupérés, filtrez-les par genre
+        filterGamesByGenre(gamesData);
+  
+        // Mettez à jour l'état pour tous les jeux
+        setAllGames(gamesData);
+      } catch {
+        setError("Impossible de charger les jeux.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchGames();
+  }, []);  // Vous pouvez ajuster la dépendance ici si nécessaire
+
+  
+  const toggleCategory = (category) => {
+    setVisibleCategory((prevState) => ({
+      ...prevState,
+      [category]: !prevState[category],
+    }));
+  };
+
+  console.log("Catégorie :", fightingGames);
 
   return (
-    <div>
-      <h1>Catalogue</h1>
-      <p>Bienvenue dans la section Catalogue.</p>    
-      <GameCardCategory  image="/logo.png"  title="CyberPunk" note={4.5} tags={genres} />
-    </div>
-  );
-}
+    <section>
+      <h1 style={styles.title}>Catalogue de jeux</h1>
 
+      {loading ? (
+        <p style={styles.loadingTxt}>Chargement en cours...</p>
+      ) : error ? (
+        <p style={{ color: "red" }}>{error}</p>
+      ) : (
+        <>
+          {/* Fighting */} 
+          <GameList title={categoryName[0]} games={fightingGames} />
+          <Button variant="contained" style={styles.button}  onClick={() => toggleCategory("fighting")}>
+          {visibleCategory.fighting ? "Masquer" : "Afficher"}
+          </Button>
+          {visibleCategory.fighting && (
+            <GameCategory
+              style={styles.gameCard}
+              categoryTitle={categoryName[0]}
+              data={fightingGames}
+              podium={false}
+            />
+          )}
+
+          {/* Shooting */}
+          <GameList title={categoryName[1]} games={shooterGames} />
+          <Button variant="contained" style={styles.button}  onClick={() => toggleCategory("shooter")}>
+          {visibleCategory.shooter ? "Masquer" : "Afficher"}
+          </Button>
+          {visibleCategory.shooter && (
+            <GameCategory
+              style={styles.gameCard}
+              categoryTitle={categoryName[1]}
+              data={shooterGames}
+              podium={false}
+            />
+          )}
+
+          {/* Platform */}
+          <GameList title={categoryName[2]} games={platformGames} />
+          <Button variant="contained" style={styles.button}  onClick={() => toggleCategory("platform")}>
+          {visibleCategory.platform ? "Masquer" : "Afficher"}
+          </Button>
+          {visibleCategory.platform && (
+            <GameCategory
+              style={styles.gameCard}
+              categoryTitle={categoryName[2]}
+              data={platformGames}
+              podium={false}
+            />
+          )}
+
+          {/* RTS */}
+          <GameList title={categoryName[3]} games={rtsGames} />
+          <Button variant="contained" style={styles.button}  onClick={() => toggleCategory("rts")}>
+          {visibleCategory.rts ? "Masquer" : "Afficher"}
+          </Button>
+          {visibleCategory.rts && (
+            <GameCategory
+              style={styles.gameCard}
+              categoryTitle={categoryName[3]}
+              data={rtsGames}
+              podium={false}
+            />
+          )}
+
+          {/* RPG */}
+          <GameList title={categoryName[4]} games={rpgGames} />
+          <Button variant="contained" style={styles.button}  onClick={() => toggleCategory("rpg")}>
+          {visibleCategory.rpg ? "Masquer" : "Afficher"}
+          </Button>
+          {visibleCategory.rpg && (
+            <GameCategory
+              style={styles.gameCard}
+              categoryTitle={categoryName[4]}
+              data={rpgGames}
+              podium={false}
+            />
+          )}
+        </>
+      )}
+      
+    </section>
+  );
+};
+
+const getStyles = (theme) => ({
+  title: {
+    fontFamily: theme.typography.fontFamily,
+    color: theme.palette.text.primary,
+    textAlign: "center",
+    margin: "2em 0 1em 0",
+  },
+  loadingTxt: {
+    fontFamily: theme.typography.fontFamily,
+    fontWeight: "700",
+    color: theme.palette.text.primary,
+    textAlign: "center",
+  },
+  button: {
+    float: "right",
+    margin: "-4em 2em 5em 0",
+    backgroundColor: theme.palette.colors.purple,
+    fontWeight: "300",
+  },
+  gameCard: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 export default CataloguePage;
